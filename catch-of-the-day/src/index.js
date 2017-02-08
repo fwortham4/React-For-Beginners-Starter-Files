865193629fb340fd713cc249ab871272d909cac9
fwortham4 <forrest.wortham@gmail.com>
2017-02-08 15:28:50 -0600

    Connects and imports React to index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,12 @@
-// let's go!
+import React from 'react'; // Imports react package and adds bundle.js to index.html head.
+import { render } from 'react-dom'; // Imports ONLY the render method from the react-dom package.
+
+// Component - like a custom HTML tag.
+class StorePicker extends React.Component {
+	// Require at least 1 (ES6) method (typically render())
+	render() {
+		return <p>Hello</p> // JSX
+	}
+}
+
+render(<StorePicker/>, document.querySelector('#main'));
