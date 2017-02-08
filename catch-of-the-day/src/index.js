c0fdf189dc51a2211c0be84e53043fb8139e0b7f
fwortham4 <forrest.wortham@gmail.com>
2017-02-08 15:34:09 -0600

    Moves StorePicker to the Components folder but still renders in index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'; // Imports react package and adds bundle.js to index.html head.
 import { render } from 'react-dom'; // Imports ONLY the render method from the react-dom package.
 
-// Component - like a custom HTML tag.
-class StorePicker extends React.Component {
-	// Require at least 1 (ES6) method (typically render())
-	render() {
-		return <p>Hello</p> // JSX
-	}
-}
+import StorePicker from './components/StorePicker';
 
 render(<StorePicker/>, document.querySelector('#main'));
