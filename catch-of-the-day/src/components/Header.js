874b0b312e0123c6eda02523b79a77052fe70a4f
fwortham4 <forrest.wortham@gmail.com>
2017-02-09 13:28:07 -0600

    Converts Header.js from a react component to a stateless function.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render() {
-		 return ( 
-		 	<header className="top">
-		 		<h1>Catch of the Day</h1>
-		 		<h3 className="tagline">{this.props.tagline}</h3>
-		 	</header>
-		 )
-	}
+// Stateless Function - could be a component, but no changes will ever be made to the Header.
+function Header(props) {
+	 return ( 
+	 	<header className="top">
+	 		<h1>
+	 			Catch
+	 			<span className="ofThe">
+	 				<span className="of">of</span>
+	 				<span className="the">the</span>
+	 			</span>
+	 			Day
+	 		</h1>
+	 		<h3 className="tagline">{props.tagline}</h3>
+	 	</header>
+	 )
 }
 
 export default Header;
