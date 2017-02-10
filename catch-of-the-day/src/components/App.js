a833a154bc73552e4ea36796585aec70a9583293
fwortham4 <forrest.wortham@gmail.com>
2017-02-10 15:29:27 -0600

    Uses state to transfer input from addFishForm to the App.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,7 +4,29 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-	
+	// Initializes State:
+	constructor() {
+		super(); // Initializes the React component so that we can access "this".
+		this.addFish = this.addFish.bind(this); // Weird ES6 bullsh#$%t to make addFish method work for App.
+		this.state = {
+			// Creates initial state for fish and order.
+			fishes: {},
+			order: {}
+		};
+	}
+
+	// Updates Fish State: 
+	addFish(fish) {
+		// 1. SET State (aka copy init state)
+		const fishes = {...this.state.fishes}; // ES6 "Spread" = ...
+		// 2. Add in new fish state.
+		const timestamp = Date.now(); // Used for ordering and incrementing of fish objects
+		fishes[`fish-${timestamp}`] = fish; // Passes info to update state with.
+		// 3. UPDATE State
+		this.setState({ fishes }) // -OR- ({ fishes: fishes })
+		// fishes(STATE): fishes(OBJ of old and new fish) - Telling which part of State to update.
+	}
+
 	render() {
 		return ( 
 			<div className="catch-of-the-day">
@@ -12,7 +34,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 			)
 	}
