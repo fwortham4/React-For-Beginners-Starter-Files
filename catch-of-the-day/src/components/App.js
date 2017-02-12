dee3ead19cd86bcd617aa021a2ae75b6a0d1ef88
fwortham4 <forrest.wortham@gmail.com>
2017-02-12 16:39:06 -0600

    Loads sample fish into the App component via state and props.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 	// Initializes State:
 	constructor() {
 		super(); // Initializes the React component so that we can access "this".
-		this.addFish = this.addFish.bind(this); // Weird ES6 bullsh#$%t to make addFish method work for App.
+		this.addFish = this.addFish.bind(this); // Weird ES6 bullsh#$%t to make "this" work for addFish method.
+		this.loadSamples = this.loadSamples.bind(this); // Weird ES6 bullsh#$%t to make "this" work for loadSamples method.
 		this.state = {
 			// Creates initial state for fish and order.
 			fishes: {},
@@ -27,6 +29,12 @@ class App extends React.Component {
 		// fishes(STATE): fishes(OBJ of old and new fish) - Telling which part of State to update.
 	}
 
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return ( 
 			<div className="catch-of-the-day">
@@ -34,7 +42,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 			)
 	}
